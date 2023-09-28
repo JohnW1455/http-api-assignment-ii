4ec43e8fdf1b1189a7fe223b024d6de53c92a02e
JohnW1455 <jhw5517@g.rit.edu>
2023-09-28 15:49:44 -0400

    fixed some eslint errors

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,48 +1,44 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-    const headers = {
-      'Content-Type': 'application/json',
-    };
-    
-    response.writeHead(status, headers);
-    response.write(JSON.stringify(object));
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    const headers = {
-      'Content-Type': 'application/json',
-    };
-  
-    response.writeHead(status, headers);
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
-    const responseJSON = {
-      users,
-    };
-  
-    return respondJSON(request, response, 200, responseJSON);
+  const responseJSON = {
+    users,
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
-  }
+  };
 
   return respondJSON(request, response, 404, responseJSON);
 };
 
-const getUsersMeta = (request, response) => {
-  return respondJSONMeta(request, response, 200);
-};
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 
-const notFoundMeta = (request, response) => {
-  return respondJSONMeta(request, response, 404);
-};
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
 
 const addUser = (request, response, body) => {
   const responseJSON = {
@@ -53,7 +49,7 @@ const addUser = (request, response, body) => {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
   }
-  
+
   let responseCode = 204;
 
   if (!users[body.name]) {
@@ -73,9 +69,9 @@ const addUser = (request, response, body) => {
 };
 
 module.exports = {
-    getUsers,
-    notFound,
-    getUsersMeta,
-    notFoundMeta,
-    addUser,
+  getUsers,
+  notFound,
+  getUsersMeta,
+  notFoundMeta,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -7,60 +7,59 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const parseBody = (request, response) => {
-    const body = [];
+  const body = [];
 
-    request.on('error', (err) => {
-      console.dir(err);
-      response.statusCode = 400;
-      response.end();
-    });
-  
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
-  
-    request.on('end', () => {
-      const bodyString = Buffer.concat(body).toString();
-      const bodyParams = query.parse(bodyString);
-  
-      jsonHandler.addUser(request, response, bodyParams);
-    });
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    jsonHandler.addUser(request, response, bodyParams);
+  });
 };
 
 const urlStruct = {
-    'GET': {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        '/getUsers': jsonHandler.getUsers,
-        '/notReal': jsonHandler.notFound,
-        notFound: jsonHandler.notFound,
-    },
-    'HEAD': {
-        '/getUsers': jsonHandler.getUsersMeta,
-        '/notReal': jsonHandler.notFoundMeta,
-        notFound: jsonHandler.notFoundMeta,
-    },
-    'POST': {
-        '/addUser': parseBody,
-    }
-}
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notFound,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notFoundMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+  POST: {
+    '/addUser': parseBody,
+  },
+};
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    const params = query.parse(parsedUrl.query);
-    const acceptedTypes = request.headers.accept.split(',');
+  const parsedUrl = url.parse(request.url);
 
-    if (!urlStruct[request.method]) {
-        return urlStruct['HEAD'].notFound(request, response);
-    }
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD.notFound(request, response);
+  }
 
-    if(urlStruct[request.method][parsedUrl.pathname]){
-        urlStruct[request.method][parsedUrl.pathname](request, response);
-    } else {
-        urlStruct[request.method].notFound(request, response);
-    }
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+  return true;
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
